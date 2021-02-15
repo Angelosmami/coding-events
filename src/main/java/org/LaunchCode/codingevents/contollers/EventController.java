200cbfceca450dc88bad996dde736ca2269a61ae
Jennifer <Angelosmami@yahoo.com>
2021-02-14 22:33:44 -0800

    finished lesson

--- src/main/java/org/LaunchCode/codingevents/contollers/EventController.java
+++ src/main/java/org/LaunchCode/codingevents/contollers/EventController.java
@@ -3,7 +3,9 @@ package org.LaunchCode.codingevents.contollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,23 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    public static List<String>events = new ArrayList<>();
+
     @GetMapping
     public String event(Model model){
-      List <String> events = new ArrayList<>();
-        events.add("Mardi Gras");
-        events.add("Valentines");
-        events.add("Easter");
         model.addAttribute("events", events);
         return "events/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEventsForm(){
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect: ";
     }
 
 
