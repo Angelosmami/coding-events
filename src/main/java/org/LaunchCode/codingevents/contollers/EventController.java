ea9b7d2bfd789b92e0f8cc66041e434816f1d961
Jennifer <Angelosmami@yahoo.com>
2021-02-14 19:15:20 -0800

    first commit

---
+++ src/main/java/org/LaunchCode/codingevents/contollers/EventController.java
@@ -0,0 +1,26 @@
+package org.LaunchCode.codingevents.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String event(Model model){
+      List <String> events = new ArrayList<>();
+        events.add("Mardi Gras");
+        events.add("Valentines");
+        events.add("Easter");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+
+}
